2efeba633fcffd2309651be29db4253aa5e15ce3
Miftahul Haq <ciftah12@gmail.com>
2025-10-01 11:50:05 +0700

    Update header.blade.php

--- resources/views/components/back/header.blade.php
+++ resources/views/components/back/header.blade.php
@@ -4,16 +4,17 @@
 <meta name="csrf-token" content="{{ csrf_token() }}">
 <meta http-equiv="X-UA-Compatible" content="ie=edge" />
 <title>{{ $title }}</title>
+@vite(['resources/tabler-dist/libs/jsvectormap/dist/jsvectormap.js'])
 {{-- GLOBAL MANDATORY STYLES --}}
 @vite(['resources/tabler-dist/css/tabler.min.css'])
 {{-- ADDITIONALS --}}
-{{-- @vite(['resources/tabler-dist/css/tabler-flags.css'])
 @vite(['resources/tabler-dist/css/tabler-flags.css'])
 @vite(['resources/tabler-dist/css/tabler-socials.css'])
 @vite(['resources/tabler-dist/css/tabler-payments.css'])
 @vite(['resources/tabler-dist/css/tabler-vendors.css'])
 @vite(['resources/tabler-dist/css/tabler-marketing.css'])
-@vite(['resources/tabler-dist/css/tabler-themes.css']) --}}
+@vite(['resources/tabler-dist/css/tabler-themes.css'])
+@vite(['resources/tabler-dist/css/demo.min.css'])
 <style>
   @import url("https://rsms.me/inter/inter.css");
   @import url("https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css");
