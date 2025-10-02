857a440958e58b36b891a5e1c024c9f21d2fb9a9
Miftahul Haq <ciftah12@gmail.com>
2025-10-02 15:53:25 +0700

    Update template back

--- resources/views/components/theme/back.blade.php
+++ resources/views/components/theme/back.blade.php
@@ -14,14 +14,19 @@
 
 <body class="{{ $pushmenuState === 'collapsed' ? 'sidebar-collapse' : '' }}">
   <script src="https://cdn.jsdelivr.net/npm/@tabler/core@1.4.0/dist/js/tabler-theme.min.js" async defer></script>
-  
+
   <div class="page">
     <x-back.navbar />
-    @yield('container')
+
+    <div class="page-wrapper">
+      @yield('container')
+      <x-back.footer />
+    </div>
   </div>
-  
+
   <x-modal.umum />
   @yield('modals')
+
   <x-back.script />
   @yield('js_bawah')
 </body>
