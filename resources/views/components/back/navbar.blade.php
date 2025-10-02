3cb2e884f8bff221eb66636b04020ae5b16972f7
Miftahul Haq <ciftah12@gmail.com>
2025-10-02 14:13:24 +0700

    Update navbar.blade.php

--- resources/views/components/back/navbar.blade.php
+++ resources/views/components/back/navbar.blade.php
@@ -1,45 +1,20 @@
-<!-- BEGIN NAVBAR  -->
 <header class="navbar navbar-expand-md d-print-none">
   <div class="container-xl">
-    <!-- BEGIN NAVBAR TOGGLER -->
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbar-menu"
       aria-controls="navbar-menu" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
-    <!-- END NAVBAR TOGGLER -->
-    <!-- BEGIN NAVBAR LOGO -->
+
     <div class="navbar-brand navbar-brand-autodark d-none-navbar-horizontal pe-0 pe-md-3">
       <a href="/" class="d-flex align-items-center text-decoration-none text-reset" aria-label="Sinkad">
-        <img src="{{ asset('favicon.ico') }}" alt="Sinkad" width="32" height="32" class="me-2">
+        <img src="{{ asset('favicon.ico') }}" alt="Sinkad" width="26" height="26" class="me-2">
         <span class="fw-bold">Sinkad</span>
       </a>
     </div>
 
 
-    <!-- END NAVBAR LOGO -->
     <div class="navbar-nav flex-row order-md-last">
       <div class="nav-item d-none d-md-flex me-3">
-        {{-- <div class="btn-list">
-          <a href="https://github.com/tabler/tabler" class="btn btn-5" target="_blank" rel="noreferrer">
-            <!-- Download SVG icon from http://tabler.io/icons/icon/brand-github -->
-            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"
-              stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
-              class="icon icon-2">
-              <path
-                d="M9 19c-4.3 1.4 -4.3 -2.5 -6 -3m12 5v-3.5c0 -1 .1 -1.4 -.5 -2c2.8 -.3 5.5 -1.4 5.5 -6a4.6 4.6 0 0 0 -1.3 -3.2a4.2 4.2 0 0 0 -.1 -3.2s-1.1 -.3 -3.5 1.3a12.3 12.3 0 0 0 -6.2 0c-2.4 -1.6 -3.5 -1.3 -3.5 -1.3a4.2 4.2 0 0 0 -.1 3.2a4.6 4.6 0 0 0 -1.3 3.2c0 4.6 2.7 5.7 5.5 6c-.6 .6 -.6 1.2 -.5 2v3.5" />
-            </svg>
-            Source code
-          </a>
-          <a href="https://github.com/sponsors/codecalm" class="btn btn-6" target="_blank" rel="noreferrer">
-            <!-- Download SVG icon from http://tabler.io/icons/icon/heart -->
-            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"
-              stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
-              class="icon text-pink icon-2">
-              <path d="M19.5 12.572l-7.5 7.428l-7.5 -7.428a5 5 0 1 1 7.5 -6.566a5 5 0 1 1 7.5 6.572" />
-            </svg>
-            Sponsor
-          </a>
-        </div> --}}
       </div>
       <div class="d-none d-md-flex">
         <div class="nav-item">
@@ -1189,4 +1164,3 @@
     </div>
   </div>
 </header>
-<!-- END NAVBAR  -->
