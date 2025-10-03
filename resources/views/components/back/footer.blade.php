8d791ee4ba309be546960af033b24676175641aa
Miftahul Haq <ciftah12@gmail.com>
2025-10-03 09:09:39 +0700

    Update footer.blade.php

--- resources/views/components/back/footer.blade.php
+++ resources/views/components/back/footer.blade.php
@@ -5,22 +5,20 @@
       <div class="col-lg-auto ms-lg-auto">
         <ul class="list-inline list-inline-dots mb-0">
           <li class="list-inline-item"><a href="https://docs.tabler.io" target="_blank" class="link-secondary"
-              rel="noopener">Documentation</a></li>
-          <li class="list-inline-item"><a href="./license.html" class="link-secondary">License</a></li>
+              rel="noopener">Dokumentasi</a></li>
           <li class="list-inline-item">
-            <a href="https://github.com/tabler/tabler" target="_blank" class="link-secondary" rel="noopener">Source
-              code</a>
-          </li>
-          <li class="list-inline-item">
-            <a href="https://github.com/sponsors/codecalm" target="_blank" class="link-secondary" rel="noopener">
-              <!-- Download SVG icon from http://tabler.io/icons/icon/heart -->
+            <a href="https://github.com/stiepemb/" target="_blank" class="link-secondary" rel="noopener">
+              Made with
               <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"
                 stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
                 class="icon text-pink icon-inline icon-4">
                 <path d="M19.5 12.572l-7.5 7.428l-7.5 -7.428a5 5 0 1 1 7.5 -6.566a5 5 0 1 1 7.5 6.572" />
               </svg>
-              Sponsor
+              IT STIE Pembangunan
             </a>
+          </li>
+          <li class="list-inline-item">
+            <a href="./changelog.html" class="link-secondary" rel="noopener"> v1.4.0 </a>
           </li>
         </ul>
       </div>
@@ -28,12 +26,11 @@
       <div class="col-12 col-lg-auto mt-3 mt-lg-0">
         <ul class="list-inline list-inline-dots mb-0">
           <li class="list-inline-item">
-            Copyright &copy; 2025
-            <a href="." class="link-secondary">Tabler</a>. All rights reserved.
+            Copyright &copy; {{ now()->year }}
+            <a href="https://stie-pembangunan.ac.id/" class="link-secondary">STIE Pembangunan Tanjungpinang</a>.
+            All rights reserved
           </li>
-          <li class="list-inline-item">
-            <a href="./changelog.html" class="link-secondary" rel="noopener"> v1.4.0 </a>
-          </li>
+
         </ul>
       </div>
     </div>
