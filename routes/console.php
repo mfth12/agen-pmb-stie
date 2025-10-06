f972d904f670efe7fac4ef23f8cf03b77559e96e
Miftahul Haq <ciftah12@gmail.com>
2025-10-06 10:24:39 +0700

    Update console.php

--- routes/console.php
+++ routes/console.php
@@ -8,8 +8,3 @@ $password = env('DB_PASSWORD');
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote')->hourly();
-
-// Artisan::command('cleandb', function () use ($password) {
-//     exec("docker exec -i ctpegawai-database mysql -u root -p{$password} -e 'DROP DATABASE sinkadstie; CREATE DATABASE sinkadstie;'");
-//     exec("docker exec -i ctpegawai-database mysql -u root -p{$password} -e 'DROP DATABASE dbaConsole; CREATE DATABASE dbaConsole;'");
-// })->purpose('Menghapus seluruh database');
