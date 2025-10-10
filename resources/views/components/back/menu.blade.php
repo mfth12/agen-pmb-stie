093142c3341d98606c1b094f2ee470a1948f4104
Miftahul Haq <ciftah12@gmail.com>
2025-10-10 23:22:59 +0700

    Update menu.blade.php

--- resources/views/components/back/menu.blade.php
+++ resources/views/components/back/menu.blade.php
@@ -90,29 +90,6 @@
                       <a class="dropdown-item" href="./lists.html"> Lists </a>
                       <a class="dropdown-item" href="./modals.html"> Modals </a>
                     </div>
-                    <div class="dropdown-menu-column">
-                      <a class="dropdown-item" href="./markdown.html"> Markdown </a>
-                      <a class="dropdown-item" href="./navigation.html"> Navigation </a>
-                      <a class="dropdown-item" href="./offcanvas.html"> Offcanvas </a>
-                      <a class="dropdown-item" href="./pagination.html"> Pagination </a>
-                      <a class="dropdown-item" href="./placeholder.html"> Placeholder </a>
-                      <a class="dropdown-item" href="./segmented-control.html">
-                        Segmented control
-                        <span class="badge badge-sm bg-green-lt text-uppercase ms-auto">New</span>
-                      </a>
-                      <a class="dropdown-item" href="./scroll-spy.html">
-                        Scroll spy
-                        <span class="badge badge-sm bg-green-lt text-uppercase ms-auto">New</span>
-                      </a>
-                      <a class="dropdown-item" href="./social-icons.html"> Social icons </a>
-                      <a class="dropdown-item" href="./stars-rating.html"> Stars rating </a>
-                      <a class="dropdown-item" href="./steps.html"> Steps </a>
-                      <a class="dropdown-item" href="./tables.html"> Tables </a>
-                      <a class="dropdown-item" href="./tabs.html"> Tabs </a>
-                      <a class="dropdown-item" href="./tags.html"> Tags </a>
-                      <a class="dropdown-item" href="./toasts.html"> Toasts </a>
-                      <a class="dropdown-item" href="./typography.html"> Typography </a>
-                    </div>
                   </div>
                 </div>
               </li>
@@ -128,95 +105,14 @@
                   <div class="dropdown-menu-columns">
                     <div class="dropdown-menu-column">
                       <a class="dropdown-item" href="./accordion.html">
-                        Accordion
+                        Agen Sekolah
                         <span class="badge badge-sm bg-green-lt text-uppercase ms-auto">New</span>
                       </a>
-                      <a class="dropdown-item" href="./alerts.html"> Alerts </a>
-                      <div class="dropend">
-                        <a class="dropdown-item dropdown-toggle" href="#sidebar-authentication"
-                          data-bs-toggle="dropdown" data-bs-auto-close="outside" role="button"
-                          aria-expanded="false">
-                          Authentication
-                        </a>
-                        <div class="dropdown-menu">
-                          <a href="./sign-in.html" class="dropdown-item"> Sign in </a>
-                          <a href="./sign-in-link.html" class="dropdown-item"> Sign in link </a>
-                          <a href="./sign-in-illustration.html" class="dropdown-item"> Sign in with illustration
-                          </a>
-                          <a href="./sign-in-cover.html" class="dropdown-item"> Sign in with cover </a>
-                          <a href="./sign-up.html" class="dropdown-item"> Sign up </a>
-                          <a href="./forgot-password.html" class="dropdown-item"> Forgot password </a>
-                          <a href="./terms-of-service.html" class="dropdown-item"> Terms of service </a>
-                          <a href="./auth-lock.html" class="dropdown-item"> Lock screen </a>
-                          <a href="./2-step-verification.html" class="dropdown-item"> 2 step verification </a>
-                          <a href="./2-step-verification-code.html" class="dropdown-item"> 2 step verification
-                            code </a>
-                        </div>
-                      </div>
-                      <a class="dropdown-item" href="./avatars.html">
-                        Avatars
-                        <span class="badge badge-sm bg-green-lt text-uppercase ms-auto">New</span>
+                      <a class="dropdown-item" href="./alerts.html"> Alerts
+                        {{-- <span class="badge badge-sm bg-green-lt text-uppercase ms-auto">New</span> --}}
                       </a>
-                      <a class="dropdown-item" href="./badges.html"> Badges </a>
-                      <a class="dropdown-item" href="./blank.html"> Blank page </a>
-                      <a class="dropdown-item" href="./buttons.html"> Buttons </a>
-                      <div class="dropend">
-                        <a class="dropdown-item dropdown-toggle" href="#sidebar-cards" data-bs-toggle="dropdown"
-                          data-bs-auto-close="outside" role="button" aria-expanded="false">
-                          Cards
-                        </a>
-                        <div class="dropdown-menu">
-                          <a href="./cards.html" class="dropdown-item"> Sample cards </a>
-                          <a href="./card-actions.html" class="dropdown-item">
-                            Card actions
-                            <span class="badge badge-sm bg-green-lt text-uppercase ms-auto">New</span>
-                          </a>
-                          <a href="./cards-masonry.html" class="dropdown-item"> Cards Masonry </a>
-                        </div>
-                      </div>
-                      <a class="dropdown-item" href="./carousel.html"> Carousel </a>
-                      <a class="dropdown-item" href="./colors.html"> Colors </a>
-                      <a class="dropdown-item" href="./datagrid.html"> Data grid </a>
-                      <a class="dropdown-item" href="./dropdowns.html"> Dropdowns </a>
-                      <div class="dropend">
-                        <a class="dropdown-item dropdown-toggle" href="#sidebar-error" data-bs-toggle="dropdown"
-                          data-bs-auto-close="outside" role="button" aria-expanded="false">
-                          Error pages
-                        </a>
-                        <div class="dropdown-menu">
-                          <a href="./error-404.html" class="dropdown-item"> 404 page </a>
-                          <a href="./error-500.html" class="dropdown-item"> 500 page </a>
-                          <a href="./error-maintenance.html" class="dropdown-item"> Maintenance page </a>
-                        </div>
-                      </div>
-                      <a class="dropdown-item" href="./lists.html"> Lists </a>
-                      <a class="dropdown-item" href="./modals.html"> Modals </a>
-                    </div>
-                    <div class="dropdown-menu-column">
-                      <a class="dropdown-item" href="./markdown.html"> Markdown </a>
-                      <a class="dropdown-item" href="./navigation.html"> Navigation </a>
-                      <a class="dropdown-item" href="./offcanvas.html"> Offcanvas </a>
-                      <a class="dropdown-item" href="./pagination.html"> Pagination </a>
-                      <a class="dropdown-item" href="./placeholder.html"> Placeholder </a>
-                      <a class="dropdown-item" href="./segmented-control.html">
-                        Segmented control
-                        <span class="badge badge-sm bg-green-lt text-uppercase ms-auto">New</span>
-                      </a>
-                      <a class="dropdown-item" href="./scroll-spy.html">
-                        Scroll spy
-                        <span class="badge badge-sm bg-green-lt text-uppercase ms-auto">New</span>
-                      </a>
-                      <a class="dropdown-item" href="./social-icons.html"> Social icons </a>
-                      <a class="dropdown-item" href="./stars-rating.html"> Stars rating </a>
-                      <a class="dropdown-item" href="./steps.html"> Steps </a>
-                      <a class="dropdown-item" href="./tables.html"> Tables </a>
-                      <a class="dropdown-item" href="./tabs.html"> Tabs </a>
-                      <a class="dropdown-item" href="./tags.html"> Tags </a>
-                      <a class="dropdown-item" href="./toasts.html"> Toasts </a>
-                      <a class="dropdown-item" href="./typography.html"> Typography </a>
                     </div>
                   </div>
-                </div>
               </li>
             </ul>
             {{-- AKHIR NAVBAR MENU --}}
